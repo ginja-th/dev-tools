3a5e9adc840280ce1cca22f21721c0a2277b8988
Corey Mcmahon <contact@coreymcmahon.com>
2016-10-19 18:17:41 +0700

    GT-197 Complete GitHub Oauth

--- app/Http/Controllers/Tokens/GitHubController.php
+++ app/Http/Controllers/Tokens/GitHubController.php
@@ -4,19 +4,26 @@ namespace App\Http\Controllers\Tokens;
 
 use App\Http\Controllers\Controller;
 use App\Integrations\GitHub;
+use App\Repositories\Collaborators;
 use Illuminate\Http\Request;
 use App\Services\Settings;
 
 class GitHubController extends Controller
 {
     protected $github;
+    protected $collaborators;
+    protected $settings;
 
-    public function __construct(GitHub $github)
+    public function __construct(GitHub $github, Collaborators $collaborators, Settings $settings)
     {
         $this->github = $github;
+        $this->collaborators = $collaborators;
+        $this->settings = $settings;
     }
 
     /**
+     * Redirects user to GitHub to complete OAuth
+     *
      * @return mixed
      */
     public function index()
@@ -26,6 +33,8 @@ class GitHubController extends Controller
     }
 
     /**
+     * Exchanges the "code" for an actual access token
+     *
      * @param Request $request
      */
     public function exchange(Request $request)
@@ -40,6 +49,11 @@ class GitHubController extends Controller
 
         $user = $this->github->fetchUserDetails($accessToken);
 
-        dd($user);
+        if ($user && $this->collaborators->findByGithubUsername($user->login)) {
+            $this->settings->set('github_oauth_token', $accessToken);
+            return response()->json(['data' => ['message' => 'Token stored.']]);
+        }
+
+        return response()->json(['data' => ['message' => 'Token was not stored.']], 400);
     }
 }
